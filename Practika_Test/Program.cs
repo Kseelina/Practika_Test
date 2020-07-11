405b460b5bf2566e30c14ba825f6fb061e922a91
Прокопьева Ксения <malecula15@mail.ru>
2020-07-11 18:32:00 +0500

    Создание лог файла (для ведения информации о запуске программы)

--- Practika_Test/Program.cs
+++ Practika_Test/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusnessLogic;
+using Models;
 using System.Configuration;
+using NLog;
 
 namespace Practika_Test
 {
@@ -13,14 +15,19 @@ namespace Practika_Test
     {
         static void Main(string[] args)
         {
+            Logger logger = LogManager.GetCurrentClassLogger(); // объявление логера
+            logger.Info("Программа успешно запущена.");          // вывод сообщения в лог файле
+
+
             string testFolder = ConfigurationManager.AppSettings["questFolder"];
             string testFile = ConfigurationManager.AppSettings["questFile"];
 
             Metods metods = new Metods(); // создание нового экземпляра объекта
             List<string> Text = metods.GetQuestions(Path.Combine(testFolder, testFile));
             // Path.Combine - функция соединения
-            metods.SetTest(Path.Combine(testFolder, testFile));
-
+            logger.Info("Файл с вопросами найден и успешно считан.");
+            List <Question> questions =metods.SetTest(Path.Combine(testFolder, testFile));
+            logger.Info("Файл с вопросами успешно преобразован в вид понятный для программы");
             Console.ReadKey();
         }
 
