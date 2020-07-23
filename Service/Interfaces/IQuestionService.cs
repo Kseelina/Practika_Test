98742ceced824dee301c2c72cf3bb585ff2e34d8
Прокопьева Ксения <malecula15@mail.ru>
2020-07-23 17:47:48 +0500

    Работа с БД (часть 1)

--- Service/Interfaces/IQuestionService.cs
+++ Service/Interfaces/IQuestionService.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace Service.Interfaces
 {
-    interface IQuestionService
+   public interface IQuestionService : IEntityServices<QuestionBase>
     {
     }
 }
